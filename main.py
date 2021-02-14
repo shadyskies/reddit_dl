56a826633f938801f96bece283a688a8336156bc
shadyskies <mehta.naman328@gmail.com>
2021-02-14 21:54:22 +0530

    almost final pics and videos working

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ from selenium import webdriver
 import requests
 import io
 import urllib.request
+import time
 
-# class="_1oQyIsiPHYt6nx7VOmd1sz _1RYN-7H8gYctjOQeL8p2Q7 scrollerItem _3Qkp11fjcAw9I9wtLo8frE _1qftyZQ2bhqP62lbPjoGAh _2IWrSJK7OQ27rTgV_N2Zu4 Post t3_l04ex8 "
-# _1oQyIsiPHYt6nx7VOmd1sz _1RYN-7H8gYctjOQeL8p2Q7 scrollerItem _3Qkp11fjcAw9I9wtLo8frE _1qftyZQ2bhqP62lbPjoGAh _1LmKpEAguLZV4jQMgQSFVL  Post t3_lgit1o 
 
 def get_post_links(wd:webdriver, max_links, query, selector_val):
     def scroll_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     
     if selector_val:
-        url = "https://www.reddit.com/search/?q=" + query + "&sort=top"
+        url = "https://www.reddit.com/search/?q=" + query 
     else:
         url = "https://www.reddit.com/r/" + query + "/top/?t=week"
     wd.get(url)
@@ -34,32 +33,56 @@ def get_post_links(wd:webdriver, max_links, query, selector_val):
 
 
 def get_media_url(posts_a, query):
-    media_url = []
+    count = 0 
     for a in posts_a:
         print(a)
         wd.get(a)
+        time.sleep(1)
+        a_urls = wd.find_elements_by_css_selector('a')
         try:
-            gif_url = wd.find_elements_by_css_selector("a")
+            video_url = [i.get_attribute('href') for i in a_urls if 'imgur' in i.get_attribute('href') or 'gfycat' in i.get_attribute('href') or "preview" in i.get_attribute('href') ]
         except:
             pass
-        media_url = [i.get_attribute('href') for i in gif_url if 'imgur' in i.get_attribute('href') or 'gfycat' in i.get_attribute('href')]
-        # img_url = wd.find_elements_by_css_selector("a")
-        # media_url = [i.get_attribute('href') for i in img_url if '.jpg' in i.get_attribute('href') or '.png' in i.get_attribute('href')]
-        if len(media_url) == 0:
-        #     media_url = [i.get_attribute('href') for i in img_url if 'imgur' in i.get_attribute('href')]
-        #     if len(media_url) == 0:
-                continue
-        #     media_url[0] = media_url[0] + '.jpeg'
-        print(media_url)
-        # download_images(url=media_url[0], folder_path='pics/', query=query)
-        download_video(url=media_url[0], folder_path='videos/', query=query)
+        try:
+            img_url = [i.get_attribute('href') for i in a_urls if '.jpg' in i.get_attribute('href') or '.png' in i.get_attribute('href')]
+        except:
+            pass
+        
+        try:
+            print(f"video:{video_url}")
+            print(f"img:{img_url}")
+            if len(img_url) == 0:
+                img_url = [i.get_attribute('href') for i in img_url if 'imgur' in i.get_attribute('href')]
+                if len(img_url) == 0 and len(video_url) == 0:
+                    continue
+                img_url[0] = img_url[0] + '.jpeg'
+        except:
+            pass
+        try:
+            download_images(url=img_url[0], folder_path='pics/', query=query, count=count)
+            count += 1
+        except:
+            pass
+        try:
+            download_video(url=video_url[0], folder_path='videos/', query=query, count=count)
+            count += 1
+        except:
+            pass
 
-def download_video(url, folder_path, query):
+
+def download_video(url, folder_path, query, count):
     fname = url.split('/')[-1]
+    if '.jpg' in url:
+        return
     if '.gifv' in url:
         fname = fname[:-1] + '.mp4'
         url = url[:url.index(".gifv")] + ".mp4"
-    if 
+    elif 'gfycat' in url:
+        wd.get(url)
+        tmp = wd.find_elements_by_css_selector('video')
+        url = tmp[0].get_attribute('poster')
+        url = url[:-4] + '.mp4'
+        print(url)
     folder_path = os.path.join(folder_path, query)
     if os.path.exists(folder_path):
         file_path = os.path.join(folder_path, fname)
@@ -68,7 +91,7 @@ def download_video(url, folder_path, query):
         file_path = os.path.join(folder_path, fname)
     try:
         urllib.request.urlretrieve(url, file_path)
-        print(f"SUCCESS - saved {url} - as {file_path}")
+        print(f"{count}::SUCCESS - saved {url} - as {file_path}")
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
@@ -92,7 +115,7 @@ def download_images(folder_path, url, query):
             file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
         with open(file_path, 'wb') as f:
             image.save(f, "JPEG", quality=85)
-        print(f"SUCCESS - saved {url} - as {file_path}")
+        print(f"{count}::SUCCESS - saved {url} - as {file_path}")
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
@@ -104,13 +127,18 @@ if __name__ == '__main__':
     if not os.path.exists('videos'):
         os.mkdir('videos/')
     wd = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver')
-    print("##########################")
-    print("Enter 0 to download from subreddit")
-    print("Enter 1 to download from search")
-    selector_val = int(input())
-    query = input("Enter query to download: ")
-    max_links = int(input("Enter number of links: "))
-    posts_a = get_post_links(wd,max_links, query,selector_val)
-    get_media_url(posts_a, query)
-    print(f"Downloaded {len(os.listdir('videos/'+query))} files")
-    wd.quit()
+    try:
+        print("##############################################################################")
+        print("Enter 0 to download from subreddit")
+        print("Enter 1 to download from search")
+        selector_val = int(input())
+        query = input("Enter query to download: ")
+        max_links = int(input("Enter number of links: "))
+        posts_a = get_post_links(wd,max_links, query,selector_val)
+        get_media_url(posts_a, query)
+        print(f"Downloaded {len(os.listdir('pics/'+query))} files")
+        wd.quit()
+    
+    except Exception as e:
+        print(e)
+        wd.quit()
